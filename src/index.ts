70080f8b1b0eaba422c73af560cfef93da0cf26f
yuhanna kapali <yuhanna.kapali@kotuko.it>
2021-08-19 14:49:59 +0545

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import path from 'path';
+import fs from 'fs';
+
+// const express = require('express');
+
+const app = express();
+app.use(cors());
+app.use(helmet());
+
+try {
+  const accessLogStream = fs.createWriteStream(path.join(__dirname, '../log/access.log'), {
+    flags: 'a',
+  });
+  app.use(morgan('combined', { stream: accessLogStream }));
+} catch (err) {
+  console.log(err);
+}
+app.use(morgan('combined'));
+
+app.get('/ping', (_req: any, res: any) => {
+  res.send('pong');
+});
+
+app.listen(3000, () => {
+  console.log('Serer started');
+});
